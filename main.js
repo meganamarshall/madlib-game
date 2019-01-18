05818308a34f77b659008ce268f3834ad88dca3c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-01-17 23:07:47 -0800

    it looks so good!

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 function madLib() {
 
 //get the inputs by id
-var nounOneInput = document.getElementById('noun-1');
-var pluralNounOneInput = document.getElementById('plural-noun-1');
-var properNounOneInput = document.getElementById('proper-noun-1');
-var adjectiveOneInput = document.getElementById('adjective-1');
-var pluralNounTwoInput = document.getElementById('plural-noun-2');
-var nounTwoInput = document.getElementById('noun-2');
-var nounThreeInput = document.getElementById('noun-3');
+    var nounOneInput = document.getElementById('noun-1');
+    var pluralNounOneInput = document.getElementById('plural-noun-1');
+    var properNounOneInput = document.getElementById('proper-noun-1');
+    var adjectiveOneInput = document.getElementById('adjective-1');
+    var pluralNounTwoInput = document.getElementById('plural-noun-2');
+    var nounTwoInput = document.getElementById('noun-2');
+    var nounThreeInput = document.getElementById('noun-3');
 
 //get values of inputs
-var nounOne = nounOneInput.value;
-var pluralNounOne = pluralNounOneInput.value;
-var properNounOne = properNounOneInput.value;
-var adjectiveOne = adjectiveOneInput.value;
-var pluralNounTwo = pluralNounTwoInput.value;
-var nounTwo = nounTwoInput.value;
-var nounThree = nounThreeInput.value;
+    var nounOne = nounOneInput.value;
+    var pluralNounOne = pluralNounOneInput.value;
+    var properNounOne = properNounOneInput.value;
+    var adjectiveOne = adjectiveOneInput.value;
+    var pluralNounTwo = pluralNounTwoInput.value;
+    var nounTwo = nounTwoInput.value;
+    var nounThree = nounThreeInput.value;
+    
 
 //make the madlib
-var makeMadlib = 'Yesterday I went to the' + nounOne + 'because I wanted' + pluralNounOne + '. I walked on' + properNounOne + 'Street and saw' + adjectiveOne + 'dogs. At the store, they ran out of' + pluralNounTwo + ', so I bought' + nounTwo + 'instead. I love my new' + nounThree + '!';
+    var makeMadlib = 'Yesterday I went to the ' + nounOne + ' because I wanted ' + pluralNounOne + '. I walked on ' + properNounOne + ' Street and saw ' + adjectiveOne + ' dogs. At the store, they ran out of ' + pluralNounTwo + ', so I bought ' + nounTwo + ' instead. I love my new ' + nounThree + '!';
 
 //display madlib
 
-var displayMadLib = document.getElementById('displayMadLib');
-displayMadLib.textContent = makeMadLib;
+    var displayMadLib = document.getElementById('displayMadLib'); displayMadLib.textContent = makeMadlib;
 
 
 }
