ebe9c3947fbccaed0cba57f86cab4503308e696d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-01-17 20:58:42 -0800

    added inputs

---
+++ main.js
@@ -0,0 +1,18 @@
+function displayMadLib() {
+
+//get the inputs by id
+var NounOneInput = document.getElementById('noun-1');
+var PluralNounOneInput = document.getElementById('plural-noun-1');
+var ProperNounOneInput = document.getElementById('proper-noun-1');
+var AdjectiveOneInput = document.getElementById('adjective-1');
+var PluralNounTwoInput = document.getElementById('plural-noun-2');
+var NounTwoInput = document.getElementById('noun-2');
+var NounThreeInput = document.getElementById('noun-3');
+//get values of inputs
+
+//make the madlib
+
+//display madlib
+
+
+}
