43dd413929aa3cc478d810a1bb1d6be8869ebbc4
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-01-17 21:09:42 -0800

    more changes

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 function displayMadLib() {
 
 //get the inputs by id
-var NounOneInput = document.getElementById('noun-1');
-var PluralNounOneInput = document.getElementById('plural-noun-1');
-var ProperNounOneInput = document.getElementById('proper-noun-1');
-var AdjectiveOneInput = document.getElementById('adjective-1');
-var PluralNounTwoInput = document.getElementById('plural-noun-2');
-var NounTwoInput = document.getElementById('noun-2');
-var NounThreeInput = document.getElementById('noun-3');
-//get values of inputs
+var nounOneInput = document.getElementById('noun-1');
+var pluralNounOneInput = document.getElementById('plural-noun-1');
+var properNounOneInput = document.getElementById('proper-noun-1');
+var adjectiveOneInput = document.getElementById('adjective-1');
+var pluralNounTwoInput = document.getElementById('plural-noun-2');
+var nounTwoInput = document.getElementById('noun-2');
+var nounThreeInput = document.getElementById('noun-3');
 
+//get values of inputs
+var nounOne = nounOneInput.value;
+var pluralNounOne = pluralNounOneInput.value;
+var properNounOne = properNounOneInput.value;
+var adjectiveOne = adjectiveOneInput.value;
+var pluralNounTwo = pluralNounTwoInput.value;
+var nounTwo = nounTwoInput.value;
+var nounThree = nounThreeInput.value;
 //make the madlib
+var makeMadlib = 'Yesterday I went to the' + nounOne + 'because I wanted' + pluralNounOne + '. I walked on' + properNounOne + 'Street and saw' + adjectiveOne + 'dogs. At the store, they ran out of' + pluralNounTwo + ', so I bought' + nounTwo + 'instead. I love my new' + nounThree + '!';
 
 //display madlib
 
