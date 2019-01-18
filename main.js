a8b22d1cecb4f33bf203f5b975030e8c61e9b7dd
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-01-17 21:12:58 -0800

    getting closer

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function displayMadLib() {
+function madLib() {
 
 //get the inputs by id
 var nounOneInput = document.getElementById('noun-1');
@@ -17,10 +17,14 @@ var adjectiveOne = adjectiveOneInput.value;
 var pluralNounTwo = pluralNounTwoInput.value;
 var nounTwo = nounTwoInput.value;
 var nounThree = nounThreeInput.value;
+
 //make the madlib
 var makeMadlib = 'Yesterday I went to the' + nounOne + 'because I wanted' + pluralNounOne + '. I walked on' + properNounOne + 'Street and saw' + adjectiveOne + 'dogs. At the store, they ran out of' + pluralNounTwo + ', so I bought' + nounTwo + 'instead. I love my new' + nounThree + '!';
 
 //display madlib
+
+var displayMadLib = document.getElementById('displayMadLib');
+displayMadLib.textContent = makeMadLib;
 
 
 }
